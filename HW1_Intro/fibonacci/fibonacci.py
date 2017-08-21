3ece0a66c2f8287299027fc3727742f3cf3d864c
Bridge <bridgeaffiliation@gmail.com>
2017-08-21 02:58:30 -0400

    pyt

--- HW1_Intro/fibonacci/fibonacci.py
+++ HW1_Intro/fibonacci/fibonacci.py
@@ -2,8 +2,17 @@
 def calc_fib(n):
     if (n <= 1):
         return n
+    fib_list = [0, 1] #index: 0,1
+    for x in range(2, n+1):
+        fib_list.append(fib_list[x-2] + fib_list[x-1])
+    return fib_list[-1]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+flg = 0
+
+while flg == 0:
+    print('Please input a number')
+    n = int(input())
+    print(calc_fib(n))
+    print('Continue?(Y=0/N=1)')
+    flg = int(input())	
